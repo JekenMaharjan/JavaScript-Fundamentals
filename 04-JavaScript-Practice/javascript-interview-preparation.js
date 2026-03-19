194142865df3f8081eb2aeeb2d4725efbd964990
JekenMaharjan <maharjanjeken@gmail.com>
2026-03-19 08:25:29 +0545

    JS-Interview-Prep: Check if a string is Palindrome

--- 04-JavaScript-Practice/javascript-interview-preparation.js
+++ 04-JavaScript-Practice/javascript-interview-preparation.js
@@ -17,7 +17,21 @@ console.log(reverse("JEKEN MAHARJAN"));
 // reverse() reverses array
 // join('') converts back to string
 
+// ====================================================================
+// 2. Check if a String is a Palindrome
+// ====================================================================
 
+// Create a function where,
+// 1. Create const forward variable which is equal to string in lowercase.
+// 2. Create const backward variable which is equal to string in lowercase, split, reverse and then join.
+// 3. Check forward === backward and then return.
+
+function palinDrome(str) {
+    const forward = str.toLowerCase();
+    const backward = str.toLowerCase().split("").reverse().join("");
+    return (forward === backward);
+}
+console.log(palinDrome("Madam"));
 
 
 
